fc2e4ce09ec2e433996e4488da74535b1053ad0b
prabhaharan1392 <prabhaharan1392@gmail.com>
2023-04-13 22:48:22 +0530

    new change

--- loginmern/backend/server.js
+++ loginmern/backend/server.js
@@ -1,33 +1,39 @@
-require('dotenv').config()
-
-const express = require('express')
-const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts')
-const userRoutes = require('./routes/user')
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
 
 // express app
-const app = express()
-
+const app = express();
+app.use(cors());
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api/workouts', workoutRoutes)
-app.use('/api/user', userRoutes)
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+async function connectDB() {
+  mongoose.set('strictQuery', false);
+  const result = await mongoose.connect(
+    'mongodb+srv://prabhaharan1392:prabhahar6@cluster0.rk0a7ad.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+    }
+  );
+  console.log('DB connected');
+  return result;
+}
+connectDB();
+app.listen(process.env.PORT, () => {
+  console.log('listening on port', process.env.PORT);
+});
+//
